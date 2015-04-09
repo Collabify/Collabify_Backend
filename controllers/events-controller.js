bdeb59452081e69e27bf0d1ca9fb8bcc15a2fe58
Sydney Johnson <syjohnso@gmail.com>
2015-04-08 23:53:00 -0500

    EventsController fully documented; fixed an old commenting bug

--- controllers/events-controller.js
+++ controllers/events-controller.js
@@ -14,9 +14,19 @@ module.exports.MAX_EVENT_DISTANCE = 5;
 /**
  * POST /events/ - Create a new event
  *
- * @param 			req				- The client request
- * @param {Event} 	req.body.event	- The event to be created
- * @param 			res				- The server response
+ * <p>Preconditions: <br>
+ * User has logged in <br>
+ * Event does not already exist <br>
+ * User is not at an event already <br>
+ *
+ * <p>Postconditions: <br>
+ * Event is created <br>
+ * User is assigned the 'DJ' role for the event
+ *
+ * @param 			req						- The client request
+ * @param {String} 	req.headers.userid		- The user's Spotify ID
+ * @param {Event} 	req.body				- The event to be created
+ * @param 			res						- The server response
  */
 module.exports.post = function (req, res) {
 	helpers.getUser(req.headers.userid, res, function (user) {
@@ -48,9 +58,10 @@ module.exports.post = function (req, res) {
 /**
  * GET /events/ - Get all events near provided coordinates
  *
- * @param 				req 				- The client request
- * @param {Location} 	req.body.location 	- The user's current latitude and longitude coordinates
- * @param {Event[]} 	res 				- List of all nearby events
+ * @param 				req 						- The client request
+ * @param {String}		req.headers.latitude		- The user's current latitude
+ * @param {String} 		req.headers.longitude		- The user's current longitude
+ * @param {Event[]} 	res 						- List of all nearby events
  */
 module.exports.get = function (req, res) {
 	/** @todo Verify latitude and longitude get converted properly */
@@ -59,8 +70,7 @@ module.exports.get = function (req, res) {
 
 	Event
 		.find()
-		.select(null)
-		//.select('name eventId location')
+		.select('name eventId location')
 		.where('location')
 		.within()
 		/** @todo Verify that this is a correct spatial query */
